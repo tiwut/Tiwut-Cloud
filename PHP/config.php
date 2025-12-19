19001c2546c543c857f0fc95df77b7538a0a2867
Till Wuttke <145930346+tiwut@users.noreply.github.com>
2025-12-19 15:15:58 +0100

    Create config.php

---
+++ PHP/config.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'admin_password' => 'Admin123', 
+    'app_name'       => 'TiwutCloud',
+    'storage_dir'    => __DIR__ . '/storage',
+    'data_dir'       => __DIR__ . '/data',
+];
